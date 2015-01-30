e210afac337b60edc1610d0cea1901f51e1473ad
Ubuntu <ubuntu@ip-172-31-59-144.ec2.internal>
2015-01-30 05:17:58 +0000

    Now gets alerts from db via django models based on price thresholds and exchange

--- getAlerts.py
+++ getAlerts.py
@@ -8,3 +8,17 @@ django.setup()
 from submitAlertJSON.models import Alert
 
 
+def getActiveEmailAlerts(price, exchangeName):
+    alertsLessThanPrice = Alert.objects.filter(priceThreshold__gte=price, sign='lessThan', exchange=exchangeName)
+    alertsGreaterThanPrice = Alert.objects.filter(priceThreshold__lte=price, sign='greaterThan', exchange=exchangeName)
+    listOfAlerts = []
+    for alert in alertsLessThanPrice:
+        listOfAlerts.append(alert)
+    
+    for alert in alertsGreaterThanPrice:
+        listOfAlerts.append(alert)
+
+    return listOfAlerts
+
+#alerts = getActiveEmailAlerts(220, 'coinbase')
+#print alerts
