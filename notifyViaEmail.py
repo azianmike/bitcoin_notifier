517e283c6b0fe1612e01a94fc69bf11a7b7ac7e5
Ubuntu <ubuntu@ip-172-31-59-144.ec2.internal>
2015-01-30 03:20:49 +0000

    Added mandrill smtp function

--- notifyViaEmail.py
+++ notifyViaEmail.py
@@ -7,6 +7,30 @@ except ImportError:
     raise ImportError('Make a UsernameAndPassword.py file with your username and password')
 
 
+def sendEmailUsingMandrill(recipient, message):
+    """
+    Takes in a loginUsername and loginPassword (used to login into Gmail SMTP
+    Also takes in a recipient (to send notification to) and a message
+    """
+
+    if recipient == None or recipient == '' or '@' not in recipient or '.' not in recipient:
+        raise AssertionError("Recipient email address it not valid")
+
+    sender = 'from@fromdomain.com'
+    receivers = [recipient]
+
+    message = 'From:'+sender+'\r\nTo:'+recipient+'\r\nSubject: Bitcoin notifier \n\n' + message
+
+
+    try:
+       smtpObj = smtplib.SMTP('smtp.mandrillapp.com:587')
+       smtpObj.starttls()
+       smtpObj.login(UsernameAndPassword.mandrillUsername, UsernameAndPassword.mandrillPassword)
+       smtpObj.sendmail(sender, receivers, message)
+    except smtplib.SMTPException:
+       raise smtplib.SMTPException("Cannot connected to smtp")
+
+
 
 def sendEmailUsingGmailSMTP(recipient, message):
     """
